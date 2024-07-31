ac726387c708cb8e19b6c3b6fc434f4bb79dc415
gdessy <giovanni.dessy@iit.it>
2024-07-31 11:31:45 +0200

    config edit

--- quadruped_pympc/config.py
+++ quadruped_pympc/config.py
@@ -6,7 +6,7 @@ from quadruped_pympc.helpers.quadruped_utils import GaitType
 
 # These are used both for a real experiment and a simulation -----------
 # These are the only attributes needed per quadruped, the rest can be computed automatically ----------------------
-robot = 'aliengo'  # 'go2', 'aliengo', 'hyqreal', 'mini_cheetah'  aliengo_arm # TODO: Load from robot_descriptions.py
+robot = 'aliengo'  # 'go2', 'aliengo', 'hyqreal', 'mini_cheetah' # TODO: Load from robot_descriptions.py
 robot_leg_joints = dict(FL=['FL_hip_joint', 'FL_thigh_joint', 'FL_calf_joint',],  # TODO: Make configs per robot.
                         FR=['FR_hip_joint', 'FR_thigh_joint', 'FR_calf_joint',],
                         RL=['RL_hip_joint', 'RL_thigh_joint', 'RL_calf_joint',],
