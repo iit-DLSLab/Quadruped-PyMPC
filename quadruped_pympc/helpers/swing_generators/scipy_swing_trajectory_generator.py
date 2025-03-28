f8bf801884dda2e22b5d1265a4d83564d40e8539
gturrisi <giulio.turrisi@iit.it>
2025-03-28 12:01:24 +0100

    tuned landing point reflex

--- quadruped_pympc/helpers/swing_generators/scipy_swing_trajectory_generator.py
+++ quadruped_pympc/helpers/swing_generators/scipy_swing_trajectory_generator.py
@@ -21,15 +21,17 @@ class SwingTrajectoryGenerator:
         scaling_factor = 1.5
         
         if early_stance_hitmoment != -1:# and early_stance_hitmoment < self.swing_period*0.9:
+            reflex_maximum_height = 0.2 # this should be per robot basis
+            
             p1 = x0.copy()
             p1[:2] = x0[:2] - 0.01 * (xf[:2]-x0[:2])
             p1 += np.array([0., 0., self.stepHeight / scaling_factor])
-            p2 = 0.5 * (x0 + xf) + np.array([0., 0., 0.2 ])
-            p3 = xf + np.array([0.0, 0.0, self.stepHeight / scaling_factor])
+            p2 = 0.5 * (x0 + xf) + np.array([0., 0., reflex_maximum_height])
+            p3 = xf + np.array([0.0, 0.0, reflex_maximum_height / scaling_factor])
 
             x = np.array([x0[0], p1[0], p2[0], p3[0], xf[0]])
             y = np.array([x0[1], p1[1], p2[1], p3[1], xf[1]])
-            z = np.array([x0[2], p1[2], p2[2], p3[2], xf[2] + 0.05])
+            z = np.array([x0[2], p1[2], p2[2], p3[2], xf[2] + reflex_maximum_height / (scaling_factor+0.5)])
 
             updated_swing_period = self.swing_period - early_stance_hitmoment
             t = np.array([early_stance_hitmoment, early_stance_hitmoment+updated_swing_period/4, early_stance_hitmoment+updated_swing_period/2, early_stance_hitmoment+updated_swing_period*3/4, self.swing_period])
