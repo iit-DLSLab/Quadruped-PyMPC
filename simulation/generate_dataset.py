dd6695c4399c11f66fd029065560b275ff2b86a1
gturrisi <giulio.turrisi@iit.it>
2025-04-18 09:32:22 +0200

    added possibility to convert to npy dataset

--- simulation/generate_dataset.py
+++ simulation/generate_dataset.py
@@ -5,6 +5,7 @@ import os
 import pathlib
 import sys
 from pprint import pprint
+import numpy as np
 
 from gym_quadruped.quadruped_env import QuadrupedEnv
 from gym_quadruped.utils.data.h5py import H5Reader
@@ -22,9 +23,12 @@ if __name__ == "__main__":
     storage_path = pathlib.Path(__file__).parent.parent / "datasets"
     data_path = run_simulation(
         qpympc_cfg=qpympc_cfg,
-        num_seconds_per_episode=1,
-        num_episodes=5,
-        render=False,
+        num_seconds_per_episode=50,
+        num_episodes=1,
+        ref_base_lin_vel=(-0.5, 0.5),
+        ref_base_ang_vel=(-0.3, 0.3),
+        base_vel_command_type="human",
+        render=True,
         recording_path=storage_path,
     )
 
@@ -45,6 +49,30 @@ if __name__ == "__main__":
         f"Number of trajectories: {dataset.len()} \n"
         f"Dataset observations names: \n{obs_names}"
     )
+
+
+    # We can also convert the dataset to a npy file
+    n_trajs = dataset.n_trajectories
+    for traj_id in range(n_trajs):
+        obs_t = {obs_name: dataset.recordings[obs_name][traj_id] for obs_name in reproduced_env.state_obs_names}
+        base_pos = obs_t['base_pos']
+        base_ori_quat_xyzw = np.roll(obs_t['base_ori_quat_wxyz'],-1)
+        qpos_js = obs_t['qpos_js']
+        fps = 1./reproduced_env.mjModel.opt.timestep
+        data = {
+            "joints_list": ["FL_hip_joint", "FL_thigh_joint", "FL_calf_joint", 
+                            "FR_hip_joint", "FR_thigh_joint", "FR_calf_joint", 
+                            "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint", 
+                            "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint"],
+            "joint_positions": [row for row in qpos_js],
+            'root_position': [row for row in base_pos],
+            'root_quaternion': [row for row in base_ori_quat_xyzw],
+            "fps": fps,
+        }
+        # Save the data to an .npy file
+        output_file = f"datasets/traj_{traj_id}.npy"
+        np.save(output_file, data)
+
 
     # And since we use the same names as QuadrupedEnv, we can get the group representations for free
     from gym_quadruped.utils.quadruped_utils import configure_observation_space_representations
