407bf09a3adee2cdfe81f452e1ff931c001d9a8a
giulioturrisi <giulioturrisi@hotmail.it>
2025-04-28 13:26:11 +0200

    cleaning

--- ros2/msgs_ws/install/dls2_msgs/share/dls2_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(dls2_msgs_IDL_FILES "msg/BlindStateMsg.idl;msg/BaseStateMsg.idl;msg/ControlSignalMsg.idl;msg/TrajectoryGeneratorMsg.idl")
-set(dls2_msgs_INTERFACE_FILES "msg/BlindStateMsg.msg;msg/BaseStateMsg.msg;msg/ControlSignalMsg.msg;msg/TrajectoryGeneratorMsg.msg")
